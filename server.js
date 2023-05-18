53890bb784a9e08e6c6b08ea552c11a3124ce402
TiknockTokomak <103266154+TiknockTokomak@users.noreply.github.com>
2023-05-18 16:16:09 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+// const http = require('http'),
+//     fs = require('fs'),
+//     url = require('url');
+
+//     http.createServer((requeest, response) => {
+//         let addr = request.url,
+//             q = url.parse(addr, true),
+//             filePath = '';
+
+//         fs.appendFile('log.txt', 'URL: ' + addr )
+//     })
+
+const url = require('url');
+const http = require('http');
+const fs = require('fs');
+http.createServer((request, response) =>{
+    let addr = request.url;
+    let q = url.parse(addr,true);
+    let filePath = '';
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        filePath = 'index.html';
+    } 
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+    })
+
+    response.writeHead(200,{'Content-Type': 'text/plain'});
+    response.write(data);
+    response.end();
+}).listen(8080);
+console.log('My test Server is running on Port 8080.');
+
